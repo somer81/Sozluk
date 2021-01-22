bd3cda61281b2c0d5522092eccee39a5870f0817
somer81 <sevinc.omer@gmail.com>
2021-01-23 00:06:59 +0300

    the dictionary first commit

---
+++ MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sozluk
+{
+    public class MyDictionary<Tkey,Tval>
+    {
+        Tkey[] _keys;
+        Tval[] _values;
+
+        Tkey[] _tempkeys;
+        Tval[] _tempvals; 
+
+
+
+        public MyDictionary()
+        {
+            _keys = new Tkey[0];
+            _values = new Tval[0];
+        }
+
+        public void Add(Tkey key, Tval value)
+        {
+            _tempkeys = _keys;
+            _tempvals = _values;
+
+            _keys = new Tkey[_keys.Length + 1];
+            _values = new Tval[_values.Length + 1];
+
+            for (int i = 0; i < _keys.Length; i++)
+            {
+                _keys[i] = _tempkeys[i];
+                _values[i] = _values[i]; 
+            }
+
+            _keys[_keys.Length - 1] = key;
+            _values[_values.Length - 1] = value;
+
+        }
+
+
+    }
+}
